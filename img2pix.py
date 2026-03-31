e14a0e3b6a3a403b636d8e8e87731c6fb764c788
Domar <omerserifoduncu@gmail.com>
2026-03-31 21:45:33 +0300

    resimleri byte arraye çevirme şimdilik çalışmıyor

--- img2pix.py
+++ img2pix.py
@@ -1,8 +1,32 @@
-from PIL import image
+from PIL import Image
+palim = Image.open("egapalette.png")
+palet = palim.getcolors()
+bytestr = ""
+#print (palet)
 
 #resim dosyasının yolunu aşağı girin:
-im = image.open("title.png")
-pix = im.load()
+im = Image.open("title.png")
 
 
-input("bir tuşa basıp çıkın")
+for x in range(0,im.size[0]):
+ for y in range(0,im.size[1]):
+  pixel = im.getpixel((x,y))
+  istrans=True
+  for i in range(0, 16):
+   #tmp = palet[i][1]
+   #tmp = tmp[:-1]
+   #print()
+   #print(palet[i][1])
+   #print(tmp)
+   #print(pixel)
+   if tuple(palet[i][1]) == pixel[:-1]:
+    istrans=False
+    bytestr += hex(i)
+    bytestr += "h,"
+   if istrans:
+    bytestr += "0x10h,"
+
+bytestr = bytestr[:-1]
+print(bytestr)
+
+input("entera basıp çıkın")
